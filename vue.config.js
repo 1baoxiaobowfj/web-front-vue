5579f7bca3b8209bc12ba99c1943529e2745b26f
baoxiaobo <15002394812@163.com>
2019-12-14 21:13:42 +0800

    第一次保存

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/mytest': {
+        target: 'http://192.168.10.123:3001',
+        changOrigin: true,
+        pathRequiresRewrite: {
+          "^/mytest": "/mytest"
+        }
+      }
+    }
+  },
 }
